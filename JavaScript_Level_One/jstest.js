bc288809d0c67999cebc0639e910a99294b8c963
mgallimore88 <mgallimore88@gmail.com>
2019-09-09 23:52:46 +0100

    Bootstrap project coffee lover and javascript basics

---
+++ JavaScript_Level_One/jstest.js
@@ -0,0 +1,8 @@
+var weightInPounds = null;
+var weightInKg = null;
+weightInPounds = prompt('Enter weight in pounds:: ');
+weightInKg = 0.454 * weightInPounds;
+
+alert("Weight in kg = " + weightInKg);
+
+console.log("Conversion Completed");
