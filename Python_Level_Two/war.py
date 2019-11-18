dd1e06ebd9ca660eaaf1730c048f2ddc1414ecc2
mgallimore88 <mgallimore88@gmail.com>
2019-11-18 06:44:12 +0000

    implemented tuple unpacking and list comprehension in deck initialisation function

--- Python_Level_Two/war.py
+++ Python_Level_Two/war.py
@@ -2,8 +2,8 @@
 ### WELCOME TO YOUR OOP PROJECT #####
 #####################################
 
-# For this project you will be using OOP to create a card game. This card game will
-# be the card game "War" for two players, you an the computer. If you don't know
+# For this project I used OOP to create a card game. This card game is the card game "War" 
+# for two players, you an the computer. If you don't know
 # how to play "War" here are the basic rules:
 #
 # The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
@@ -28,7 +28,6 @@
 
 from random import shuffle
 
-# Two useful variables for creating Cards.
 suits = "H D S C".split()
 ranks = "2 3 4 5 6 7 8 9 10 J Q K A".split()
 
@@ -36,17 +35,16 @@ ranks = "2 3 4 5 6 7 8 9 10 J Q K A".split()
 class Deck:
     """
     This is the Deck Class. This object will create a deck of cards to initiate
-    play. You can then use this Deck list of cards to split in half and give to
-    the players. It will use SUITE and RANKS to create the deck. It should also
-    have a method for splitting/cutting the deck in half and Shuffling the deck.
+    play.
     """
 
     def __init__(self, suits, ranks):
-        self.cards = []
+        self.cards = [(s, r) for s in suits for r in ranks]
         self.played_cards = []
-        for suit in suits:
-            for rank in ranks:
-                self.cards.append(str(suit + rank))
+        print(self.cards)
+        # for suit in suits:
+        #     for rank in ranks:
+        #         self.cards.append(str(suit + rank))
 
     def shuffle(self):
         shuffle(self.cards)
@@ -59,7 +57,7 @@ class Deck:
 class Hand:
     """
     This is the Hand class. Each player has a Hand, and can add or remove
-    cards from that hand. There should be an add and remove card method here.
+    cards from that hand.
     """
 
     def __init__(self, cards):
@@ -72,7 +70,13 @@ class Hand:
         [self.cards.insert(0, x) for x in cards]
 
     def draw(self):
-        return self.cards.pop(-1)
+        try:
+            return self.cards.pop(-1)
+        except IndexError:
+            print("No cards left")
+            game_on = False
+        finally:
+            pass
 
 
 class Player:
@@ -115,9 +119,9 @@ class Player:
 
 # base level functions
 def rank(card):
-    rank = card[1:]
+    rank = card[1]
     if rank.isnumeric():
-        return int(card[1:])
+        return int(card[1])
     elif rank == "J":
         return 11
     elif rank == "Q":
