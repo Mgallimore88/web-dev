5f73ca4c2ae5fc2b84231671b9669d78cb5ea1a2
mgallimore88 <mgallimore88@gmail.com>
2020-02-02 17:00:54 +0000

    removes project4 template exercise from mike branch to allow code along

--- Django_Level_Four/learning_templates/basic_app/views.py
+++ Django_Level_Four/learning_templates/basic_app/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request,'basic_app/index.html')
 
 def other(request):
-    return render(request,'basic_app/other.html')
+    return render(request, 'basic_app/other.html')
 
 def relative(request):
-    return render(request,'basic_app/relative_url_templates.html')
+    return render(request, 'basic_app/relative_url_templates.html')
